43b76fac78064a4a41a432c1f6ae8a21ae009b75
zenix-s <sergioffdev@gmail.com>
2025-04-15 16:29:56 +0200

    up

---
+++ CPP03/ex01/Utils.cpp
@@ -0,0 +1,14 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+class Utils
+{
+  private:
+  public:
+    Utils();
+    Utils(const Utils& other);
+    ~Utils();
+    Utils& operator=(const Utils& other);
+};
+
+#endif
